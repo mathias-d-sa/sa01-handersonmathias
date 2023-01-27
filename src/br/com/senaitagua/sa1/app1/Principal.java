9b67a78a1f4e587dbb4f7fd5aef98be051d810e0
mathias-d-sa <handersonmathias4@gmail.com>
2023-01-27 17:59:04 -0300

    iniciando desafio 2 da sa01

---
+++ src/br/com/senaitagua/sa1/app1/Principal.java
@@ -0,0 +1,29 @@
+package br.com.senaitagua.sa1.app1;
+
+import javax.swing.JOptionPane;
+
+import br.com.senaitagua.sa1.bo.CalculoBO;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double valor1, valor2;
+		
+		JOptionPane.showMessageDialog(null, "Empresa Voe - Grupo Empresarial");
+		
+		//Entrada de Dados:
+		valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do primeiro avião: ")); 
+		valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do segundo avião: "));
+		
+		//Passar uma mensagem (método) para o Objeto:
+		CalculoBO cbo = new CalculoBO();
+		
+		// Utilizar o Objeto
+		JOptionPane.showMessageDialog(null, cbo.Somar(valor1, valor2));
+		
+		
+
+	}
+
+}
---
+++ src/br/com/senaitagua/sa1/bo/CalculoBO.java
@@ -0,0 +1,9 @@
+package br.com.senaitagua.sa1.bo;
+
+public class CalculoBO {
+	
+	public double Somar(double n1, double n2) {
+		return n1 + n2;
+	}
+
+}
